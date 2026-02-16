b5e11f0bc37a4e3efcc11be330ef54adae243d43
aman sanganti <amansanganti30@gmail.com>
2026-02-16 15:48:34 +0530

    lecture 2

--- code.cpp
+++ code.cpp
@@ -2,10 +2,69 @@
 using namespace std;
 
 int main(){
-    int age;
-    cin >> age;
-    cout << age << " years old" << endl;
+    // cout << sizeof(age) << " bytes" << endl;
+    
+    // int age = 25;
+    // char grade = 'A';
+    // float PI = 3.14f;
+    // bool isPassed = true;
+    // double price = 99.99;
+    // cout << age << " " << grade << " " << PI << " " << isPassed << " " << price << endl;
 
+    // If boolean value is true, it will print 1, otherwise it will print 0.
+
+    // char grade = 'A';
+
+    // int value = grade; // Implicit conversion from char to int
+    // cout << value << endl;
+    // The ASCII value of 'A' is 65, so it will print 65.The ASCII value will be getting priinted
+
+    // double price = 99.99;
+
+    // int Newprice = int(price);
+    // cout << price << " " << Newprice << endl;
+
+    // int age;
+    // cout << "Enter Your age: ";
+    // cin >> age;
+    // cout<< "You entered: " << age << endl;
+
+
+    // int a = 19;
+    // int b = 5;
+    // cout << "Addition"<< " " <<"a + b = " << a+ b << endl;
+    // cout << "Subtraction"<< " " <<"a - b = " << a - b << endl;
+    // cout << "Multiplication"<< " " <<"a * b = " << a * b << endl;
+    // cout << "Division"<< " " <<"a / b = " << a / b << endl; // Integer division
+    // cout << "Modulo"<< " " <<"a % b = " << a % b << endl; // Modulo operator gives the remainder of the division
+
+    // int c = 5;
+    // float d = 2;
+    // cout << "c / d = " << c / d << endl;
+
+    // cout << (5/(double)2) << endl;
+
+    // cout << (3 < 5) << endl; // False = 0
+    // cout << (3 > 5) << endl; // True = 1
+    // cout << (3 == 5) << endl; // False = 0
+    // cout << (3 != 5) << endl; // True = 1
+    // cout << (3 <= 5) << endl; // True = 1
+    // cout << (3 >= 5) << endl; // False = 0
+
+
+    // Sum of 2 numbers
+    // int num1, num2;
+    // cout << "Enter two numbers: ";
+    // cin >> num1 >> num2;
+    // cout << "Sum of " << num1 << " and " << num2 << " is: " << num1 + num2 << endl;
+
+
+    int a = 10;
+
+    int b = a++;
+
+    cout << b << endl;
+    cout << a << endl;
     return 0;
-}
 
+}
